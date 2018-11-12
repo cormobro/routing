284bab367e8b031caeae2f19f2bb65411612ca68
cormobro <bonaet.felix@gmail.com>
2018-11-12 16:20:31 +0100

    ajout navlinks

---
+++ src/History.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class History extends Component {
+    render() {
+        return (
+            <div>
+             Notre histoire </div>
+        );
+    }
+}
+
+export default History;
